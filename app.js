be2d5cf607c549eeca9b5c12eb2a718a2c03835c
Hamza Mujinovic <haminimi.dev@gmail.com>
2024-02-19 00:44:06 +0100

    Add the CORS middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const mongoConfig = require('./mongoConfig');
 
@@ -11,6 +12,7 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
