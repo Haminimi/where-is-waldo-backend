82b9d49a12c1c1d4a0ee59315b804ce1976f8edb
Hamza Mujinovic <haminimi.dev@gmail.com>
2024-02-20 20:27:03 +0100

    Add the scores router to the Express app

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const createError = require('http-errors');
 const express = require('express');
+const createError = require('http-errors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -8,19 +8,22 @@ const cors = require('cors');
 const mongoConfig = require('./mongoConfig');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const scoresRouter = require('./routes/score');
 
 const app = express();
 
 app.use(cors());
-app.use(logger('dev'));
+if (process.env.NODE_ENV === 'development') {
+	app.use(logger('dev'));
+}
+app.use(express.text());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/scores', scoresRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
